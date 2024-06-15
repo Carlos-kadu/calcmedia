5c618fd86f0fb95e589b571cb26c9c688316aabe
Carlos Eduardo Rodriges <eduardokadu831@gmail.com>
2024-06-15 14:37:30 -0300

    atualizações no search

--- assets/js/search.js
+++ assets/js/search.js
@@ -10,12 +10,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const data = [{
             name: 'Cálculo 1 - Bernardini',
             url: 'c1-bernardini',
-            tags: ['cálculo', 'bernardini', 'c1']
+            tags: ['cálculo 1', 'bernardini', 'c1']
         },
         {
             name: 'Matemática Discreta 2 - Bernardini',
             url: 'md2-bernardini',
-            tags: ['matemática discreta', 'bernardini', 'md2']
+            tags: ['matemática discreta 2', 'bernardini', 'md2']
         },
         {
             name: 'Eng. Econômica - Glauceny',
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
         {
             name: 'Cálculo 2 - Yoko',
             url: 'c2-yoko',
-            tags: ['cálculo', 'yoko', 'c2']
+            tags: ['cálculo 2', 'yoko', 'c2']
         },
         {
             name: 'IAL - Yoko',
@@ -35,47 +35,47 @@ document.addEventListener('DOMContentLoaded', function() {
         {
             name: 'Matemática Discreta 1 - Glauco',
             url: 'md1-glauco',
-            tags: ['md1', 'matemática discreta', 'glauco']
+            tags: ['md1', 'matemática discreta 1', 'glauco']
         },
         {
             name: 'Estrutura de dados e algoritmos 1 - Rose',
             url: 'eda1-rose',
-            tags: ['eda1', 'estrutura de dados', 'algoritmos', 'rose']
+            tags: ['eda1', 'estrutura de dados e algoritmos 1', 'algoritmos', 'rose']
         },
         {
             name: 'Estrutura de dados e algoritmos 1 - Nilton',
             url: 'eda1-nilton',
-            tags: ['eda1', 'estrutura de dados', 'algoritmos', 'nilton']
+            tags: ['eda1', 'estrutura de dados e algoritmos 1', 'algoritmos', 'nilton']
         },
         {
             name: 'Matemática Discreta 1 - Edson',
             url: 'md1-edson',
-            tags: ['md1', 'matemática discreta', 'edson']
+            tags: ['md1', 'matemática discreta 1', 'edson']
         },
         {
             name: 'Teoria de eletrônica digital 1 - Renato',
             url: 'ted1-renato',
-            tags: ['ted1', 'teoria de eletrônica', 'digital', 'renato', 'eletrônica']
+            tags: ['ted1', 'teoria de eletrônica digital 1', 'digital', 'renato', 'eletrônica']
         },
         {
             name: 'Prática de eletrônica digital 1 - Anderson',
             url: 'ped1-anderson',
-            tags: ['ped1', 'prática de eletrônica', 'digital', 'anderson', 'eletrônica']
+            tags: ['ped1', 'prática de eletrônica digital 1', 'digital', 'anderson', 'eletrônica']
         },
         {
             name: 'Fundamentos de Arquitetura de Computadores - John',
             url: 'fac-john',
-            tags: ['fac', 'john', 'arquitetura', 'fundamentos', 'computadores']
+            tags: ['Fundamentos de Arquitetura de Computadores', 'fac', 'john', 'arquitetura', 'fundamentos', 'computadores']
         },
         {
             name: 'Probabilidade e Estatística aplicado à Engenharia - Marília',
             url: 'peae-marilia',
-            tags: ['pe', 'peae', 'marilia', 'probabilidade']
+            tags: ['Probabilidade e Estatística aplicado à Engenharia', 'pe', 'peae', 'marilia', 'probabilidade']
         },
         {
             name: 'Estrutura de dados e algoritmos 2 - John',
             url: 'eda2-john',
-            tags: ['eda2', 'john', 'estrutura de dados', 'algoritmos']
+            tags: ['eda2', 'john', 'estrutura de dados e algoritmos 2', 'algoritmos']
         },
         {
             name: 'Compiladores - Bruno Ribas',
@@ -85,7 +85,12 @@ document.addEventListener('DOMContentLoaded', function() {
         {
             name: 'Sistemas de Banco de Dados 1 - Vandor Roberto',
             url: 'sbd1-vandor',
-            tags: ['vandor', 'sbd1', 'bancos', 'dados']
+            tags: ['Sistemas de Banco de Dados 1', 'vandor', 'sbd1', 'bancos', 'dados']
+        },
+        {
+            name: 'Fundamentos de Sistemas Operacionais - Daniel',
+            url: 'fso-daniel',
+            tags: ['Fundamentos de Sistemas Operacionais', 'fso', 'daniel', 'sistemas', 'fundamentos', 'operacionas']
         }
     ];
     /* INSERIR NOVA DISCIPLINA ACIMA */
@@ -112,29 +117,38 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             const minCharsMessage = document.createElement('p');
             minCharsMessage.textContent = 'Por favor, digite pelo menos 2 caracteres para fazer uma busca.';
+            minCharsMessage.classList.add('txt-body');
             modalBody.appendChild(minCharsMessage);
         }
 
         if (!haResultados && termoBusca.length >= 2) {
             const noResults = document.createElement('p');
             noResults.textContent = 'Nenhum resultado encontrado.';
+            noResults.classList.add('txt-body');
             modalBody.appendChild(noResults);
         }
 
         $('#resultadosModal').modal('show');
     }
 
-    // Verifique se valorRecebido não é nulo ou vazio antes de fazer a pesquisa
-    if (valorRecebido && valorRecebido.trim() !== '') {
-        searchInput.value = valorRecebido;
-        pesquisar(valorRecebido);
+    if (valorRecebido.trim() == '') {
+        const minCharsMessage = document.createElement('p');
+        minCharsMessage.textContent = 'Por favor, digite pelo menos 2 caracteres para fazer uma busca.';
+        minCharsMessage.classList.add('txt-body');
+        modalBody.appendChild(minCharsMessage);
     }
 
-    searchButton.addEventListener('click', function(event) {
-        event.preventDefault();
-        const termoBusca = searchInput.value;
-        pesquisar(termoBusca);
-    });
+    // Verifique se valorRecebido não é nulo ou vazio antes de fazer a pesquisa
+    if (valorRecebido && valorRecebido.trim() !== '') {
+        const interval = setInterval(() => {
+            const searchInput = document.querySelector('#search-input');
+            if (searchInput) {
+                clearInterval(interval);
+                searchInput.value = valorRecebido;
+                pesquisar(valorRecebido);
+            }
+        }, 100);
+    }
 
     function enviaDisciplina(url) {
         var valor = url;
